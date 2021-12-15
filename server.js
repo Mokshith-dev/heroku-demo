cb81886ff31c81a62b25db583330dc2caaea8463
Mokshith-dev <mokshith.ajalade>
2021-12-15 22:18:49 +0530

    Initial commit

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ app.get("/api/movie", async function (req, res) {
 });
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  // const path = require("path");
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 }
 // start the server listening for requests
 app.listen(process.env.PORT || 3001, () => console.log("Server is running..."));
